e0efec0c0338dc2895092f7d546f16384223ddf7
Lewis <52398329+Darknoon5891@users.noreply.github.com>
2021-01-21 23:16:33 +0000

    Added main initial commit

---
+++ util/Importer/main.py
@@ -0,0 +1,95 @@
+# Importer for Project Synthesis
+# This importer will allow items to be added into the source code with no coding required
+
+class Main():
+    def __init__(self):
+
+        global os, time 
+        import os, time
+
+        global ut, add_object
+        import util as ut
+        import add_object
+
+        #run pre required functions
+        Main.getbuild_mcversion()
+
+        print("This script is designed to add items into the mod source code")
+        print("if you have moved this file outside of the git file structure errors will occur")
+        print("currently only adding items are supported")
+        print("Current mod mc version:", buildMCVersion)
+        print("")
+
+        Main.main_interface()
+
+    def main_interface():
+        #Main Options
+        print("What operation do you require:")
+        print("Enter [A]dd to add an object")
+        print("Enter [E]dit to edit an object")
+        print("Enter [D]elete to remove an object")
+
+        enter = input()
+        enter = enter.upper()
+        os.system("cls")
+
+        Main.nav(enter)
+
+    def nav(code):
+
+        directory = {
+            "A" : add_object.Interface,
+            "E" : "edit_object.Interface",
+            "D" : "remove_object.Interface",
+        }
+
+        try:
+            call = directory.get(code)
+            call()
+        except Exception as e:
+            ut.message("rnv")
+            time.sleep(1)
+            os.system("cls")
+            Main.main_menu()
+
+    def getbuild_mcversion():
+        
+        global buildMCVersion
+
+        cwd = os.getcwd()  # Get the current working directory (cwd)
+        files = os.listdir(cwd)  # Get all the files in that directory
+        print("Files in %r: %s" % (cwd, files))
+        
+        path = ["../../src/main/resources/mcmod.info"]
+        for i in range(len(path)):
+            a = open(path[i], "r")
+            aRead = a.read()
+            a.close()
+            
+            try:
+                x = (aRead.index('"mcversion": ') + 13)
+                buildMCVersion = aRead[x:]
+
+                #expects to encounter close quotation mark within 10 chars 
+                y = 0
+                for i in range(10):
+                    if buildMCVersion[i] == '"': # if it finds the mark break
+                        y = y + 1
+                    elif y == 2: # expects opening mark encounter
+                        buildMCVersion = buildMCVersion[:i]
+                        break
+
+            except ValueError:
+                print("mod version unknown")
+                buildMCVersion = "unknown"
+                break
+
+            break
+
+        return buildMCVersion
+        
+
+if __name__ == "__main__":
+    print("please run via start")
+else:
+    Main()
