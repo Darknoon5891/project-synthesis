127bd2de5f0164f778cda56649a3a5192b017b53
Lewis <52398329+Darknoon5891@users.noreply.github.com>
2021-01-29 07:27:25 +0000

    added object get info and modual structer

--- util/Importer/add_object.py
+++ util/Importer/add_object.py
@@ -1,41 +1,78 @@
 
 class Interface():
     def __init__(self):
-        
         #imports
-        global os, time, ut, main
+        global os, time, ut, menu
 
         import os, time
-
         import util as ut
         import menu
 
         while True:
             print("Adding Object")
-            print("Do you want to add a [b]lock or [i]tem")
-            enterType = input()
+            print("Do you want to add a [B]lock or [I]tem")
+            entertype = input()
+            entertype = entertype.upper()
 
-            enterType = enterType.lower()
-
-            if enterType == "b":
-                AddBlock()
-            elif enterType == "i":
-                AddItem()
+            if entertype[0] == "B":
+                Interface.add_object_get_info("block")
+            elif entertype[0] == "I":
+                Interface.add_object_get_info("item")
             else:
                 ut.message("rnv")
                 
 
-class AddItem():
-    def __init__(self):
-        print("Adding Item")
+    def add_object_get_info(object_type):
+        print("Adding", object_type)
+        
+        try:
+            print("What is the", object_type, "name")
+            print("e.g. Magnite Gem")
+            object_name = input("")
+            os.system("cls")
+            ut.input_validation(object_name, "objectName")
+        except ValueError:
+            ut.message("rnv")
 
-    #add item def
+        print(object_type, "name:", object_name)
 
-class AddBlock():
-    def __init__(self):
-        print("Adding Block")
+        try:
+            while True:
+                print("Confim the texture file is in the correct graphics folder")
+                print("or you can move the file into the folder and reload to continue")
+                print("[Y]es or [R]eload")
+                
+                conformation = input("")
+                conformation = conformation.upper()
+
+                if conformation[0] != "Y":
+                    print(os.listdir("../graphics"))
+                    break
+                else:
+                    os.system("cls")
+
+        except Exception:
+            ut.message("rnv")
+
+        try:
+            Item() #make item
+        except Exception:
+            ut.message("e")
     
-    #add block def
+
+        
+class Item():
+    def __init__(self):
+        print("Adding Item")       
+
+    #add item to itemArray (ModItems)
+    #add item base
+
+    #add lang
+    #add model
+    #add texture
+
+
 
 
 if __name__ == "__main__":
